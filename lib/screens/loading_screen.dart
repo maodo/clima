55f5fbd02fdec5bad6fa0abdc71b06d66214f2eb
maodo <36578673+maodo@users.noreply.github.com>
2022-05-20 16:27:05 +0000

    Geolocator plugin integration and issues fix on IOS platform

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:geolocator/geolocator.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,13 +11,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<void> getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           child: Text('Get location'),
-          onPressed: () => {},
+          onPressed: () => {
+            getLocation()
+          },
         ),
       ),
     );
